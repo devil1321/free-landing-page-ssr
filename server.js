75c5492f5324ae25ce0f6c20249f9b1b8d8d3efe
devil1321 <s.dominik1321@gmail.com>
2021-06-19 13:32:40 +0200

    srr init landing-page

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const cors = require('cors')
 // register view engine
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views');
 // middleware & static files
+app.use(cors())
 app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, '/views')));
 app.use(express.urlencoded({extended:true}))
